606f8cdcefa0aa46abc68184f5a4ffa9fd10cd8b
Carlos Dueñas <duenascarlos9@gmail.com>
2024-05-08 15:45:56 -0500

    Cambios en la creacion de usuarios

--- api/views.py
+++ api/views.py
@@ -31,12 +31,16 @@ def all_users(request):
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        user = User.objects.get(username=serializer.data['username'])
-        user.set_password(serializer.data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        email = serializer.validated_data.get('email')
+        try:
+            user = User.objects.get(email= email)
+            return Response({'error':'El usuario ya existe'}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            user = serializer.save()
+            user.set_password(serializer.data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
